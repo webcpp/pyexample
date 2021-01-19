64e1896b4eb7c2cf707ef3bc29d2e47a960368d3
pangpang@hi-nginx.com <pangpang@hi-nginx.com>
2021-01-19 21:51:57 +0800

    update

--- website/index.py
+++ website/index.py
@@ -1,26 +1,26 @@
 import pymysql
-import pymysql.cursors
 from dbutils.pooled_db import PooledDB
 
 def get_config():
     return {'host':'127.0.0.1',
-    'port':3306,
-    'username':'root',
-    'password':'123456',
-    'database':'testdb',
-    'charset':'utf8mb4'}
+            'port':3306,
+            'user':'root',
+            'password':'123456',
+            'database':'testdb',
+            'charset':'utf8mb4',
+            'creator':pymysql,
+            'autocommit':True,
+            'blocking':False,
+            'cursorclass':pymysql.cursors.DictCursor,
+            'mincached':0,#the default of 0 means no connections are made at startup
+            'maxcached':0,#the default value of 0 or None means unlimited pool size
+            'maxconnections':0,#the default value of 0 or None means any number of connections)
+            }
 
 class dbhelp:
     def __init__(self):
         self.__config = get_config()
-        self.__pool = PooledDB(pymysql,
-                        cursorclass=pymysql.cursors.DictCursor,
-                        host=self.__config['host'],
-                        port=self.__config['port'],
-                        user=self.__config['username'],
-                        password=self.__config['password'],
-                        database=self.__config['database'],
-                        charset=self.__config['charset'])
+        self.__pool = PooledDB(**self.__config)
         self.__connection = self.__pool.connection()
         self.__cursor = self.__connection.cursor()
     def execute(self,sql,*argv):
